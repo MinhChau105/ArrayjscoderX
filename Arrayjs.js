4ce0445edc6bc7caa3f7f5de71dc47422eeeb0c1
MinhChau105 <30284931+MinhChau105@users.noreply.github.com>
2019-11-25 23:46:31 +0700

    Add files via upload

---
+++ Arrayjs.js
@@ -0,0 +1,18 @@
+// tao ds lop
+
+var hs1 = { name: 'Chau', age: 19, chucvu: 'hs'};
+var hs2 = { name: 'Quang', age: 19, chucvu: 'lop truong'};
+var hs3 = { name: 'Long', age: 19, chucvu: 'lop pho hoc tap'};
+var hs4 = { name: 'Phuong', age: 19, chucvu: 'lop pho ky luat'};
+var hs5 = { name: 'Hoang', age: 19, chucvu: 'hs'};
+var hs6 = { name: 'Vu', age: 19, chucvu: 'hs'};
+
+
+var dslopA1 = [
+hs1,
+hs2,
+hs3,
+hs4,
+hs5,
+hs6
+];
