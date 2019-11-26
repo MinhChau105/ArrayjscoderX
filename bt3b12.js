be7c201ff04abc472f615112a137eb291982de6f
MinhChau105 <30284931+MinhChau105@users.noreply.github.com>
2019-11-27 01:41:43 +0700

    Add files via upload

---
+++ bt3b12.js
@@ -0,0 +1,8 @@
+var a = '1000';
+var b = '200';
+
+console.log(a > b);
+// Chạy chương trình và tìm hiểu vì sao kết quả lại là false mặc dù 1000 trông có vẻ lớn hơn 200. Viết câu trả lời ở dưới đây.
+
+
+// vì đây là so sánh chuỗi với chuỗi chứ k phải là số (bảng mã ACSII)
